34a14e010df1ad82260d7df71f7fa23e4fb5cc29
ThreadSurfer <giladij@icloud.com>
2022-08-04 23:16:36 -0700

    modified css for video centering on smaller screens. Removed name from bio (already in title)

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,9 +1,7 @@
-import {React, useState} from 'react'
+import {React} from 'react'
 import profileImage from "../img/selfie.jpg"
 
 export default function Bio() {
-
-    const [infoVisible, setInfoVisible] = useState("hidden")
 
   return (
     <div id="bio">
@@ -11,7 +9,7 @@ export default function Bio() {
     <img id="selfie" src={profileImage} alt="selfie picture of myself"></img>
     </div>
     
-    <p className="bioText" id = "personalName">Joel Giladi</p>
+    {/* <p className="bioText" id = "personalName">Joel Giladi</p> */}
     <p className="bioText">Recently graduated Software Developer with 2 years experience of school and freelance work.
     <br></br>
     <br></br> 
